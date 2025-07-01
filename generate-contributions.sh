0318689ac0be3f767e6867e308391a0a00868178
Fahad-Al-Maashani <122757915+Fahad-Al-Maashani@users.noreply.github.com>
2025-07-02 00:56:45 +0400

    add script file

---
+++ generate-contributions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script makes fake commits to simulate a dense contribution graph
+# Requires Git to be installed
+
+START_DATE="2024-07-01"
+END_DATE="2025-06-30"
+
+while [ "$START_DATE" != "$END_DATE" ]; do
+  for i in {1..5}; do
+    echo "Fake commit $i on $START_DATE" > file.txt
+    GIT_AUTHOR_DATE="$START_DATE 12:00:00" GIT_COMMITTER_DATE="$START_DATE 12:00:00" git commit -am "Commit #$i on $START_DATE" --allow-empty
+  done
+  START_DATE=$(date -I -d "$START_DATE + 1 day")
+done
+
